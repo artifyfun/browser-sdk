2c573993d9c86ce3fbb51d62b4cc029fa2eb012c
artifyfun <artifyfun@gmail.com>
2025-09-02 11:14:42 +0800

    支持ie11浏览器

--- packages/rum/src/entries/proxy.ts
+++ packages/rum/src/entries/proxy.ts
@@ -1,30 +1,35 @@
 // baseUrl -> proxy
 
-const { protocol, host } = globalThis.location || {}
+var location = window.location || {};
+var protocol = location.protocol;
+var host = location.host;
 
-function buildOptions(origin: any = {}) {
-  const options = Object.assign({}, origin)
+function buildOptions(origin: any) {
+  if (origin === void 0) { origin = {}; }
+  var options = { ...origin };
   if (typeof options.baseUrl !== 'string') {
-    options.baseUrl = ''
+    options.baseUrl = '';
   }
-  options.baseUrl = !options.baseUrl ? `${protocol}//${host}` : options.baseUrl
-  if (options.baseUrl.startsWith('/')) {
-    options.baseUrl = `${protocol}//${host}${options.baseUrl}`
+  options.baseUrl = !options.baseUrl ? protocol + '//' + host : options.baseUrl;
+  if (options.baseUrl.indexOf('/') === 0) {
+    options.baseUrl = protocol + '//' + host + options.baseUrl;
   }
-  options.proxy = ({ path, parameters }: { path: string, parameters: string }) => {
-    const pathAndParameters = path + '?' + encodeURIComponent(parameters)
-    return options.baseUrl.endsWith('/') ? options.baseUrl.slice(0, -1) + pathAndParameters : options.baseUrl + pathAndParameters
-  }
-  options.site = options.site || options.baseUrl
-  options.clientToken = options.clientToken || 'snc-client-token'
-  return options
+  options.proxy = function(params: { path: string, parameters: string }) {
+    var path = params.path;
+    var parameters = params.parameters;
+    var pathAndParameters = path + '?' + encodeURIComponent(parameters);
+    return options.baseUrl.slice(-1) === '/' ? options.baseUrl.slice(0, -1) + pathAndParameters : options.baseUrl + pathAndParameters;
+  };
+  options.site = options.site || options.baseUrl;
+  options.clientToken = options.clientToken || 'snc-client-token';
+  return options;
 }
 
 export function proxy(target: any) {
-  const originInit = target.init
+  var originInit = target.init;
   target.init = function(initConfiguration: any) {
-    const options = buildOptions(initConfiguration)
-    return originInit.call(this, options)
-  }
-  return target
+    var options = buildOptions(initConfiguration);
+    return originInit.call(this, options);
+  };
+  return target;
 }
